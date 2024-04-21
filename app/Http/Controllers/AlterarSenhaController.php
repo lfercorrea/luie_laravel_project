3a2f2687168e7685c9a14d006636b3741b23792a
lfercorrea <84098072+lfercorrea@users.noreply.github.com>
2024-04-21 12:34:50 -0300

    Security hotfix

--- app/Http/Controllers/AlterarSenhaController.php
+++ app/Http/Controllers/AlterarSenhaController.php
@@ -9,35 +9,45 @@ class AlterarSenhaController extends Controller
 {
     public function alterar_senha ($id) {
         $usuario = User::findOrFail($id);
-        return view('alterar_senha', [
-                'page_title' => 'Alterar senha - ' . $usuario->name,
-                'usuario' => $usuario,
-                'modo' => 'alterar',
-        ]);
+
+        if ( auth()->user()->id === $usuario->id ) {
+            
+            return view('alterar_senha', [
+                    'page_title' => 'Alterar senha - ' . $usuario->name,
+                    'usuario' => $usuario,
+                    'modo' => 'alterar',
+            ]);
+        }
+
+        return redirect()->route('site.index')->with('fail', 'Você não pode alterar a senha de outro usuário.');
     }
 
     public function store (Request $request) {
         $user = User::findOrFail($request->id);
 
-        $rules = [
-            'password' => 'required|string|confirmed|regex:/^(?=.*[a-zA-Z])(?=.*[\W_]).{8,}$/',
-            'password_confirmation' => 'required',
-        ];
-        
-        $messages = [
-            'password.required' => 'A senha é obrigatória.',
-            'password.string' => 'A senha precisa ser uma string.',
-            'password.confirmed' => 'As senhas fornecidas são diferentes.',
-            'password.regex' => 'A senha precisa ter no mínimo 8 caracteres, incluindo um símbolo ou espaço.',
-            'password_confirmation.required' => 'É necessário confirmar a senha.',
-        ];
+        if ( auth()->user()->id === $user->id ) {
+            $rules = [
+                'password' => 'required|string|confirmed|regex:/^(?=.*[a-zA-Z])(?=.*[\W_]).{8,}$/',
+                'password_confirmation' => 'required',
+            ];
+            
+            $messages = [
+                'password.required' => 'A senha é obrigatória.',
+                'password.string' => 'A senha precisa ser uma string.',
+                'password.confirmed' => 'As senhas fornecidas são diferentes.',
+                'password.regex' => 'A senha precisa ter no mínimo 8 caracteres, incluindo um símbolo ou espaço.',
+                'password_confirmation.required' => 'É necessário confirmar a senha.',
+            ];
 
-        $request->validate($rules, $messages);
+            $request->validate($rules, $messages);
 
-        $arr_user = $request->all();
-        $arr_user['password'] = bcrypt($request->password);
-        $user->update($arr_user);
+            $arr_user = $request->all();
+            $arr_user['password'] = bcrypt($request->password);
+            $user->update($arr_user);
 
-        return redirect()->route('site.index')->with('success', 'Senha alterada.');
+            return redirect()->route('site.index')->with('success', 'Senha alterada.');
+        }
+
+        return redirect()->route('site.index')->with('fail', 'Você não pode alterar a senha de outro usuário.');
     }
 }
